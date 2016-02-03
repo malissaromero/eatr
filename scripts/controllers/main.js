cb289ed37f365506e38db6ea77c595823e27a4fa
Malissa Romero <romero.malissa@gmail.com>
2016-02-03 15:30:47 -0500

    added services and json data

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,21 +2,30 @@
 
 angular
 .module('eatrApp')
-.controller('mainCtrl', ['$scope', '$firebase',
-  function($scope, $firebaseArray) {
+.controller('mainCtrl', ['$scope', 'dataService', '$firebase',
+  function($scope, dataService, $firebaseArray) {
 
     var url = "https://glowing-heat-1887.firebaseio.com/recipes";
     var fireRef = new Firebase(url);
 
     $scope.recipes = $firebaseArray(fireRef);
 
-    fireRef.set({
-      recipe1: {
-        name: "Squash Spaghetti",
-        ingredients: "Salt, Pepper, Squash",
-        calories: "400"
-      }
+    dataService.getRecipes(function(response) {
+      $scope.recipes = response.data;
     });
+
+    $scope.createRecipe = function(recipe) {
+      $scope.recipes.unshift(recipe);
+    };
+
+    $scope.deleteRecipe = function(recipe, $index) {
+      dataService.deleteRecipe(recipe);
+      $scope.recipes.splice($index, 1);
+    };
+
+    $scope.saveRecipes = function() {
+      dataService.saveRecipe(recipe);
+    }
 
     $scope.formIsVisible = false
     $scope.toggleForm = function() {
@@ -30,20 +39,3 @@ angular
 
   }
   ]);
-
-  // dataService.getRecipes(function(response) {
-  //   $scope.recipes = response.data;
-  // });
-  //
-  // $scope.addRecipe = function() {
-  //   $scope.recipes.unshift(recipe);
-  // };
-  //
-  // $scope.deleteRecipe = function(recipe, $index) {
-  //   dataService.deleteRecipe(recipe);
-  //   $scope.recipes.splice($index, 1);
-  // };
-  //
-  // $scope.saveRecipes = function() {
-  //
-  // }
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -4,17 +4,27 @@ angular
 .module('eatrApp')
 .service('dataService', function($http) {
 
+    var urlBase = 'http://localhost\\:3000';
+
   this.getRecipes = function(callback) {
-    $http.get('mock/recipes.json')
+    return $http.get('/mock/recipes.json')
     .then(callback)
   };
 
-  this.deleteRecipe = function(recipe) {
+  this.getRecipe = function(id) {
+    return $http.get(urlBase + '/' + id);
+  };
 
+  this.createRecipe = function(recipe) {
+    return $http.post(urlBase, recipe);
+  };
+
+  this.deleteRecipe = function(recipe) {
+    return $http.delete(urlBase + '/' + id);
   };
 
   this.saveRecipes = function(recipe) {
-
+    return $http.put(urlBase + '/' + id);
   };
 
 })
