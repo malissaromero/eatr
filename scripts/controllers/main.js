8dce727443a4b163af82cc7bcf20c1cc2e130af0
Malissa Romero <romero.malissa@gmail.com>
2016-02-04 13:51:55 -0500

    services now working

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,27 +1,83 @@
+// 'use strict';
+//
+// angular
+// .module('eatrApp')
+// .controller('mainCtrl', ['$scope', '$firebase',
+//   function($scope, $firebaseArray) {
+//
+//     var url = "https://eatr.firebaseio.com/recipes";
+//     var fireRef = new Firebase(url);
+//
+//     fireRef.set({
+//       "recipe1": {
+//         "name": "Squash Spaghetti",
+//         "ingredients": "Salt, Pepper, Squash",
+//         "calories" : "400"
+//       },
+//       "recipe2": {
+//         "name": "Bolongne Sandwich",
+//         "ingredients": "Cheese, Tomato, Bread",
+//         "calories": "500"
+//       }
+//     })
+//
+//     $scope.recipes = $firebaseArray(fireRef);
+//
+//     $scope.formIsVisible = false
+//     $scope.toggleForm = function() {
+//       if($scope.formIsVisible) {
+//         $scope.formIsVisible = false
+//       }
+//       else {
+//         $scope.formIsVisible = true
+//       }
+//     };
+//
+//     $scope.reset = function() {
+//       $scope.recipe.name = "";
+//       $scope.recipe.ingredients = "";
+//       $scope.recipe.calories = "";
+//     };
+//
+//     $scope.createRecipe = function() {
+//       $scope.recipes.$add({
+//         name: $scope.recipe.name,
+//         ingredients: $scope.recipe.ingredients,
+//         calories: $scope.recipe.calories
+//       });
+//       $scope.reset()
+//     };
+//
+//     this.editRecipe = function(index) {
+//       var recipe = this.recipes[index];
+//         this.name = recipe.name.$save();
+//         this.ingredients = recipe.ingredients.$save();
+//         this.calories = recipe.calories.$save();
+//     };
+//
+//     $scope.deleteRecipe = function(key, recipe) {
+//       $scope.recipes.$remove(key);
+//     };
+//
+//   }
+//   ]);
+
 'use strict';
 
 angular
 .module('eatrApp')
-.controller('mainCtrl', ['$scope', '$firebase',
-  function($scope, $firebaseArray) {
+.controller('mainCtrl', ['$scope', 'dataService', '$firebase',
+  function($scope, dataService, $firebaseArray) {
 
     var url = "https://eatr.firebaseio.com/recipes";
     var fireRef = new Firebase(url);
 
-    fireRef.set({
-      "recipe1": {
-        "name": "Squash Spaghetti",
-        "ingredients": "Salt, Pepper, Squash",
-        "calories" : "400"
-      },
-      "recipe2": {
-        "name": "Bolongne Sandwich",
-        "ingredients": "Cheese, Tomato, Bread",
-        "calories": "500"
-      }
-    })
-
     $scope.recipes = $firebaseArray(fireRef);
+
+    dataService.getRecipes(function(response) {
+      console.log(response.data)
+      $scope.recipes = response.data;
+    });
 
     $scope.formIsVisible = false
     $scope.toggleForm = function() {
@@ -40,7 +96,7 @@ angular
     };
 
     $scope.createRecipe = function() {
-      $scope.recipes.$add({
+      $scope.recipes.unshift({
         name: $scope.recipe.name,
         ingredients: $scope.recipe.ingredients,
         calories: $scope.recipe.calories
@@ -48,75 +104,14 @@ angular
       $scope.reset()
     };
 
-    this.editRecipe = function(index) {
-      var recipe = this.recipes[index];
-        this.name = recipe.name.$save();
-        this.ingredients = recipe.ingredients.$save();
-        this.calories = recipe.calories.$save();
-    };
+    $scope.saveRecipe = function(recipe) {
+      dataService.saveRecipe(recipe);
+    }
 
-    $scope.deleteRecipe = function(key, recipe) {
-      $scope.recipes.$remove(key);
+    $scope.deleteRecipe = function(index, recipe) {
+      dataService.deleteRecipe(recipe);
+      $scope.recipes.splice(index, 1);
     };
 
   }
   ]);
-
-  // 'use strict';
-  //
-  // angular
-  // .module('eatrApp')
-  // .controller('mainCtrl', ['$scope', 'dataService', '$firebase',
-  //   function($scope, dataService, $firebaseArray) {
-  //
-  //     var url = "https://glowing-heat-1887.firebaseio.com/recipes";
-  //     var fireRef = new Firebase(url);
-  //
-  //     fireRef.set({"malissa": "isconnected"})
-  //
-  //     $scope.recipes = $firebaseArray(fireRef);
-  //
-  //     dataService.getRecipes(function(response) {
-  //       $scope.recipes = response.data;
-  //     });
-  //
-  //     $scope.formIsVisible = false
-  //     $scope.toggleForm = function() {
-  //       if($scope.formIsVisible) {
-  //         $scope.formIsVisible = false
-  //       }
-  //       else {
-  //         $scope.formIsVisible = true
-  //       }
-  //     };
-  //
-  //     $scope.reset = function() {
-  //       $scope.recipe.name = ""
-  //       $scope.recipe.phoneNumber = ""
-  //     };
-  //
-  //     $scope.createRecipe = function() {
-  //       dataService.createRecipe();
-  //       $scope.recipes.$add({
-  //         name: $scope.recipe.name,
-  //         ingredients: $scope.recipe.ingredients,
-  //         calories: $scope.recipe.calories
-  //       });
-  //       $scope.reset()
-  //     };
-  //
-  //     this.editRecipe = function(index) {
-  //       dataService.saveRecipe(recipe);
-  //       var recipe = this.recipes[index];
-  //         this.name = recipe.name.$save();
-  //         this.ingredients = recipe.ingredients.$save();
-  //         this.calories = recipe.calories.$save();
-  //     };
-  //
-  //     $scope.deleteRecipe = function(key, recipe) {
-  //       dataService.deleteRecipe(recipe);
-  //       $scope.recipes.$remove(key);
-  //     };
-  //
-  //   }
-  //   ]);
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -1,30 +1,32 @@
-// 'use strict';
-//
-// angular
-// .module('eatrApp')
-// .service('dataService', function($http) {
-//
-//   var urlBase = 'http://localhost\\:3000';
-//
-//   this.getRecipes = function(callback) {
-//     return $http.get('/mock/recipes.json')
-//     .then(callback)
-//   };
-//
-//   this.getRecipe = function(recipe) {
-//     return $http.get(urlBase + '/' + recipe);
-//   };
-//
-//   this.createRecipe = function(recipe) {
-//     return $http.post(urlBase, recipe);
-//   };
-//
-//   this.deleteRecipe = function(recipe) {
-//     return $http.delete(urlBase + '/' + recipe);
-//   };
-//
-//   this.saveRecipes = function(recipe) {
-//     return $http.put(urlBase + '/' + recipe);
-//   };
-//
-// })
+'use strict';
+
+angular
+.module('eatrApp')
+.service('dataService', function($http) {
+
+  var urlBase = 'http://localhost\\:3000';
+
+  this.getRecipes = function(callback) {
+    return $http.get('/mock/recipes.json')
+    .then(callback)
+  };
+
+  this.getRecipe = function(recipe) {
+    return $http.get(urlBase + '/' + recipe);
+  };
+
+  this.createRecipe = function(recipe) {
+    return $http.post(urlBase, recipe);
+  };
+
+  this.deleteRecipe = function(recipe) {
+    console.log("The " + recipe.name + " has been deleted.")
+    return $http.delete(urlBase + '/' + recipe);
+  };
+
+  this.saveRecipe = function(recipe) {
+    console.log("The " + recipe.name + " has been saved.")
+    return $http.put(urlBase + '/' + recipe);
+  };
+
+})
