d614acca48769c242c59a49ad1651964a3580805
Malissa Romero <romero.malissa@gmail.com>
2016-02-04 11:41:41 -0500

    http services wasn't working moved back to jquery js backend

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,30 +2,26 @@
 
 angular
 .module('eatrApp')
-.controller('mainCtrl', ['$scope', 'dataService', '$firebase',
-  function($scope, dataService, $firebaseArray) {
+.controller('mainCtrl', ['$scope', '$firebase',
+  function($scope, $firebaseArray) {
 
     var url = "https://glowing-heat-1887.firebaseio.com/recipes";
     var fireRef = new Firebase(url);
 
+    fireRef.set({
+      "recipe1": {
+        "name": "Squash Spaghetti",
+        "ingredients": "Salt, Pepper, Squash",
+        "calories" : "400"
+      },
+      "recipe2": {
+        "name": "Bolongne Sandwich",
+        "ingredients": "Cheese, Tomato, Bread",
+        "calories": "500"
+      }
+    })
+
     $scope.recipes = $firebaseArray(fireRef);
-
-    dataService.getRecipes(function(response) {
-      $scope.recipes = response.data;
-    });
-
-    $scope.createRecipe = function(recipe) {
-      $scope.recipes.unshift(recipe);
-    };
-
-    $scope.deleteRecipe = function(recipe, $index) {
-      dataService.deleteRecipe(recipe);
-      $scope.recipes.splice($index, 1);
-    };
-
-    $scope.saveRecipes = function() {
-      dataService.saveRecipe(recipe);
-    }
 
     $scope.formIsVisible = false
     $scope.toggleForm = function() {
@@ -37,5 +33,90 @@ angular
       }
     };
 
+    $scope.reset = function() {
+      $scope.recipe.name = "";
+      $scope.recipe.ingredients = "";
+      $scope.recipe.calories = "";
+    };
+
+    $scope.createRecipe = function() {
+      $scope.recipes.$add({
+        name: $scope.recipe.name,
+        ingredients: $scope.recipe.ingredients,
+        calories: $scope.recipe.calories
+      });
+      $scope.reset()
+    };
+
+    this.editRecipe = function(index) {
+      var recipe = this.recipes[index];
+        this.name = recipe.name.$save();
+        this.ingredients = recipe.ingredients.$save();
+        this.calories = recipe.calories.$save();
+    };
+
+    $scope.deleteRecipe = function(key, recipe) {
+      $scope.recipes.$remove(key);
+    };
+
   }
   ]);
+
+  // 'use strict';
+  //
+  // angular
+  // .module('eatrApp')
+  // .controller('mainCtrl', ['$scope', 'dataService', '$firebase',
+  //   function($scope, dataService, $firebaseArray) {
+  //
+  //     var url = "https://glowing-heat-1887.firebaseio.com/recipes";
+  //     var fireRef = new Firebase(url);
+  //
+  //     fireRef.set({"malissa": "isconnected"})
+  //
+  //     $scope.recipes = $firebaseArray(fireRef);
+  //
+  //     dataService.getRecipes(function(response) {
+  //       $scope.recipes = response.data;
+  //     });
+  //
+  //     $scope.formIsVisible = false
+  //     $scope.toggleForm = function() {
+  //       if($scope.formIsVisible) {
+  //         $scope.formIsVisible = false
+  //       }
+  //       else {
+  //         $scope.formIsVisible = true
+  //       }
+  //     };
+  //
+  //     $scope.reset = function() {
+  //       $scope.recipe.name = ""
+  //       $scope.recipe.phoneNumber = ""
+  //     };
+  //
+  //     $scope.createRecipe = function() {
+  //       dataService.createRecipe();
+  //       $scope.recipes.$add({
+  //         name: $scope.recipe.name,
+  //         ingredients: $scope.recipe.ingredients,
+  //         calories: $scope.recipe.calories
+  //       });
+  //       $scope.reset()
+  //     };
+  //
+  //     this.editRecipe = function(index) {
+  //       dataService.saveRecipe(recipe);
+  //       var recipe = this.recipes[index];
+  //         this.name = recipe.name.$save();
+  //         this.ingredients = recipe.ingredients.$save();
+  //         this.calories = recipe.calories.$save();
+  //     };
+  //
+  //     $scope.deleteRecipe = function(key, recipe) {
+  //       dataService.deleteRecipe(recipe);
+  //       $scope.recipes.$remove(key);
+  //     };
+  //
+  //   }
+  //   ]);
--- scripts/services/data.js
+++ scripts/services/data.js
@@ -1,30 +1,30 @@
-'use strict';
-
-angular
-.module('eatrApp')
-.service('dataService', function($http) {
-
-    var urlBase = 'http://localhost\\:3000';
-
-  this.getRecipes = function(callback) {
-    return $http.get('/mock/recipes.json')
-    .then(callback)
-  };
-
-  this.getRecipe = function(id) {
-    return $http.get(urlBase + '/' + id);
-  };
-
-  this.createRecipe = function(recipe) {
-    return $http.post(urlBase, recipe);
-  };
-
-  this.deleteRecipe = function(recipe) {
-    return $http.delete(urlBase + '/' + id);
-  };
-
-  this.saveRecipes = function(recipe) {
-    return $http.put(urlBase + '/' + id);
-  };
-
-})
+// 'use strict';
+//
+// angular
+// .module('eatrApp')
+// .service('dataService', function($http) {
+//
+//   var urlBase = 'http://localhost\\:3000';
+//
+//   this.getRecipes = function(callback) {
+//     return $http.get('/mock/recipes.json')
+//     .then(callback)
+//   };
+//
+//   this.getRecipe = function(recipe) {
+//     return $http.get(urlBase + '/' + recipe);
+//   };
+//
+//   this.createRecipe = function(recipe) {
+//     return $http.post(urlBase, recipe);
+//   };
+//
+//   this.deleteRecipe = function(recipe) {
+//     return $http.delete(urlBase + '/' + recipe);
+//   };
+//
+//   this.saveRecipes = function(recipe) {
+//     return $http.put(urlBase + '/' + recipe);
+//   };
+//
+// })
