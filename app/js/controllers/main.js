d9360e89f1c57af797c100f57a6ca3d69c8e8729
Malissa Romero <romero.malissa@gmail.com>
2016-02-24 15:25:03 -0500

    main js saved incorrectly, now fixed

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -8,21 +8,6 @@ angular
     var url = "https://eatr.firebaseio.com/recipes";
     var fireRef = new Firebase(url);
 
-    // fireRef.set({
-    //   "recipe1": {
-    //     "name": "PB&J Sandwich",
-    //     "ingredients": "Peanut Butter, Jelly, Bread"
-    //   },
-    //   "recipe2": {
-    //     "name": "Macaroni and Cheese",
-    //     "ingredients": "Cheese, Macaroni"
-    //   },
-    //   "recipe3": {
-    //     "name": "Grilled Cheese",
-    //     "ingredients": "American Cheddar, Bread"
-    //   }
-    // })
-
     $scope.recipes = $firebaseArray(fireRef);
 
     $scope.formIsVisible = false
@@ -74,66 +59,3 @@ angular
 
   }
   ])
-  .controller('apiCtrl', function($scope, $http) {
-
-    $scope.plateThreeIsVisible = false
-    $scope.togglePlateThree = function() {
-      if($scope.plateThreeIsVisible) {
-        $scope.plateThreeIsVisible = false
-      }
-      else {
-        $scope.plateThreeIsVisible = true
-      }
-    };
-
-    $scope.recipe = {
-      search: ''
-    }
-
-    $scope.fetchRecipes = function() {
-      console.log($scope.recipe.search)
-      $http.get("https://api.yummly.com/v1/api/recipes?", {
-        headers : {
-          },
-        params : {
-          "q" : $scope.recipe.search,
-          "requirePictures" : "true"}
-      })
-      .then(function(response) {
-        $scope.details = response.data;
-
-        $scope.results = [];
-        $scope.details = response.data;
-        for (var i = 0; i < 10; i++) {
-          var recipeName = response.data.matches[i].recipeName
-          $scope.results.push({title: recipeName})
-          // console.log(recipeName)
-        }
-        $scope.recipeId = {
-          id: $scope.details.matches[0].id
-        }
-        $scope.fetchRecipeId()
-      });
-    };
-
-    // $scope.recipeId = {
-    //   id: 'Bourbon-Burger-1303425'
-    // }
-
-    $scope.fetchRecipeId = function() {
-      $http.get("https://api.yummly.com/v1/api/recipe/" + $scope.recipeId.id, {
-        headers : {
-          }
-        })
-        .then(function(response) {
-          $scope.lines = [];
-          $scope.idDetails = response.data
-          for (var i = 0; i < 100; i++) {
-            var recipeIngredients = response.data.ingredientLines[i]
-            $scope.lines.push({ingredients: recipeIngredients})
-            // console.log(recipeIngredients)
-          }
-        });
-      };
-
-  });
