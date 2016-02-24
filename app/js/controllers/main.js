29c1aea025bd41f5fdede5913fce7f0309e90aa9
Malissa Romero <romero.malissa@gmail.com>
2016-02-24 14:34:36 -0500

    set up main.js

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -1,61 +1,96 @@
-'use strict';
-
+ // angular
+  //   .module('bookmark')
+  //   .controller('mainController', function($firebaseArray, BookAdder, $rootScope) {
+  //     var vm = this;
+  //
+  //     var wantToRead = new Firebase(firebaseURL   "/books/wantToRead");
+  //
+      // var wantArray = $firebaseArray(wantToRead);
+  //
+  //     vm.wantToRead = wantArray
+  //
+  //     $rootScope.$on('wantToRead', function() {
+  //       vm.wantToRead.$add( BookAdder.get() )
+  //     })
+  //
+  //   })
+  //
+  // })
 angular
 .module('eatrApp')
-.controller('mainCtrl', ['$scope', '$firebaseArray',
-  function($scope, $firebaseArray) {
+.controller('mainCtrl', function($scope, $firebaseArray, recipeAdder, $rootScope) {
 
-    var url = "https://eatr.firebaseio.com/recipes";
-    var fireRef = new Firebase(url);
+  var firebaseUrl = "https://eatr.firebaseio.com/recipes";
+  var wantToRecipe = new Firebase(firebaseUrl   "/recipes/wantToRecipe");
 
-    $scope.recipes = $firebaseArray(fireRef);
+  var wantArray = $firebaseArray(wantToRecipe);
 
-    $scope.formIsVisible = false
-    $scope.toggleForm = function() {
-      if($scope.formIsVisible) {
-        $scope.formIsVisible = false
-      }
-      else {
-        $scope.formIsVisible = true
-      }
-    };
+  $scope.wantToRecipe = wantArray
 
-    $scope.reset = function() {
-      $scope.recipe.name = "";
-      $scope.recipe.ingredients = "";
-      $scope.recipe.calories = "";
-    };
+  $rootScope.$on('wantToRecipe', function() {
+    $scope.wantToRecipe.$add( recipeAdder.get() )
+  });
 
-    $scope.createRecipe = function() {
-      $scope.recipes.$add({
-        name: $scope.recipe.name,
-        ingredients: $scope.recipe.ingredients
-      });
-      $scope.reset()
-    };
+})
 
-    $scope.recipes.$loaded(function() {
-      console.log("$loaded is on")
-     if ($scope.recipes.length === 0) {
-       $scope.recipes.$add({
-         name: "PB&J Sandwich",
-         ingredients: "Peanut Butter, Jelly, Bread"
-       });
-     }
-   });
-
-    $scope.updateRecipe = function(index) {
-      console.log("update is on")
-      var recipe = $scope.recipes[index];
-        $scope.name = recipe.name;
-        $scope.ingredients = recipe.ingredients;
-        $scope.calories = recipe.calories;
-      $scope.recipes.$save(recipe);
-    };
-
-    $scope.deleteRecipe = function(key, recipe) {
-      $scope.recipes.$remove(key);
-    };
-
-  }
-  ])
+// 'use strict';
+//
+// angular
+// .module('eatrApp')
+// .controller('mainCtrl', ['$scope', '$firebaseArray',
+//   function($scope, $firebaseArray) {
+//
+//     var url = "https://eatr.firebaseio.com/recipes";
+//     var fireRef = new Firebase(url);
+//
+//     $scope.recipes = $firebaseArray(fireRef);
+//
+//     $scope.formIsVisible = false
+//     $scope.toggleForm = function() {
+//       if($scope.formIsVisible) {
+//         $scope.formIsVisible = false
+//       }
+//       else {
+//         $scope.formIsVisible = true
+//       }
+//     };
+//
+//     $scope.reset = function() {
+//       $scope.recipe.name = "";
+//       $scope.recipe.ingredients = "";
+//       $scope.recipe.calories = "";
+//     };
+//
+//     $scope.createRecipe = function() {
+//       $scope.recipes.$add({
+//         name: $scope.recipe.name,
+//         ingredients: $scope.recipe.ingredients
+//       });
+//       $scope.reset()
+//     };
+//
+//     $scope.recipes.$loaded(function() {
+//       console.log("$loaded is on")
+//      if ($scope.recipes.length === 0) {
+//        $scope.recipes.$add({
+//          name: "PB&J Sandwich",
+//          ingredients: "Peanut Butter, Jelly, Bread"
+//        });
+//      }
+//    });
+//
+//     $scope.updateRecipe = function(index) {
+//       console.log("update is on")
+//       var recipe = $scope.recipes[index];
+//         $scope.name = recipe.name;
+//         $scope.ingredients = recipe.ingredients;
+//         $scope.calories = recipe.calories;
+//       $scope.recipes.$save(recipe);
+//     };
+//
+//     $scope.deleteRecipe = function(key, recipe) {
+//       $scope.recipes.$remove(key);
+//     };
+//
+//   }
+//   ])
