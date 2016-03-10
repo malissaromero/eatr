79ac49d1fbda9485ba0611e465675d21b3acf320
Malissa Romero <romero.malissa@gmail.com>
2016-03-10 13:38:55 -0500

    grabbing the id for the get request now works

--- app/js/controllers/api.js
+++ app/js/controllers/api.js
@@ -22,8 +22,7 @@ angular
     console.log($scope.recipe.search)
     $http.get("https://api.yummly.com/v1/api/recipes?", {
       headers : {
-        "X-Yummly-App-ID" : "fff5495f",
-        "X-Yummly-App-Key" : "e58fc1567bc839f3c927850b195a954c"},
+        },
       params : {
         "q" : $scope.recipe.search,
         "requirePictures" : "true"}
@@ -34,24 +33,25 @@ angular
       $scope.results = [];
       for (var i = 0; i < 10; i++) {
         var recipeName = response.data.matches[i].recipeName
-        $scope.results.push({title: recipeName})
+        var recipeId = response.data.matches[i].id
+        $scope.results.push({title: recipeName, id: recipeId})
       }
-      $scope.recipeId = {
-        id: $scope.details.matches[0].id
-      }
-      $scope.fetchRecipeDetails()
     });
   };
 
-  // $scope.recipeId = {
-  //   id: ''
-  // }
+  $scope.fetchRecipeDetails = function($event) {
+  // the ahref has a custom attribute that grabs the recipe id and passes the id into the click function through $event
 
-  $scope.fetchRecipeDetails = function() {
-    $http.get("https://api.yummly.com/v1/api/recipe/" + $scope.recipeId.id, {
+  // we then grab the id inside the $event and pass it to the id variable
+
+  //the id variable needs to be written this way because data-recipe-id is a revolving array and you put it in quotes because it is a name you can pass as a string
+    console.log($event)
+
+    var id = $event.currentTarget.attributes["data-recipe-id"].value
+
+    $http.get("https://api.yummly.com/v1/api/recipe/" + id, {
       headers : {
-        "X-Yummly-App-ID" : "fff5495f",
-        "X-Yummly-App-Key" : "e58fc1567bc839f3c927850b195a954c"}
+        }
       })
       .then(function(response) {
         $scope.lines = [];
