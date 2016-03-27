9bcff85ab3faf66c9c6227990bdd5024a917acfc
Malissa Romero <romero.malissa@gmail.com>
2016-03-26 21:43:59 -0400

    nav is now responsive

--- app/js/javascript/script.js
+++ app/js/javascript/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 
   $('a').click(function(){
-      $('html, body').animate({
-          scrollTop: $( $(this).attr('href') ).offset().top
-      }, 500);
-      return false;
+    $('html, body').animate({
+      scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
+
+  var menuIcon = document.querySelector('.menu-icons');
+  var menu = menuIcon.parentElement;
+  menuIcon.addEventListener('click', function(event){
+    if (menu.classList.contains('open')) {
+      menu.classList.remove('open');
+    } else {
+      menu.classList.add('open');
+    }
   });
 });
